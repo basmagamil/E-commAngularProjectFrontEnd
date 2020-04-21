4604c8ee9db0f4a2deae42239b16b9735d7bde75
basmagamil <basma.gamil17@gmail.com>
2020-04-21 13:03:25 +0200

    initial-login-register

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  @Output() loginEvent = new EventEmitter();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  loginForm = new FormGroup({
+    email:new FormControl('b',Validators.required),
+    password:new FormControl('b',Validators.required),
+  })
+
+  get emailStatus(){
+    return this.loginForm.controls.email.valid;
+  }
+  get passwordStatus(){
+    return this.loginForm.controls.password.valid;
+  }
+
+  onClickLoginSubmit(){
+    // console.log(this.registerationForm);
+    if(this.loginForm.valid){
+      this.loginEvent.emit(this.loginForm.value)
+    }
   }
 
 }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  @Output() registerEvent = new EventEmitter();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  registerForm = new FormGroup({
+    username: new FormControl('',Validators.required),
+    email:new FormControl('',Validators.required),
+    password:new FormControl('',Validators.required),
+    gender:new FormControl(),
+  })
+
+  get usernameStatus(){
+    return this.registerForm.controls.username.valid;
+  }
+  get emailStatus(){
+    return this.registerForm.controls.email.valid;
+  }
+  get passwordStatus(){
+    return this.registerForm.controls.password.valid;
+  }
+
+  onClickRegisterSubmit(){
+    if(this.registerForm.valid){
+      console.log(this.registerForm.value);
+      this.registerEvent.emit(this.registerForm.value)
+    }
   }
 
 }
